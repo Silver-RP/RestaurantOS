c84ff933d36ee5d675f6720fea735988bff80237
phamquangdai0805 <phamquangdai0805@gmail.com>
2024-12-28 23:31:32 +0700

    CRUD role and middleware

---
+++ restaurant-backend/src/controller/RoleController.ts
@@ -0,0 +1,25 @@
+import {Request, Response, NextFunction} from "express";
+import RoleService from "../services/RoleService";
+
+class RoleController {
+  async AddRole(req: Request, res: Response): Promise<void> {
+    await RoleService.AddRole(req, res);
+  }
+
+  async GetRoleById(req: Request, res: Response): Promise<void> {
+    await RoleService.GetRoleById(req, res);
+  }
+
+  async GetAllRole(req: Request, res: Response): Promise<void> {
+    await RoleService.GetAllRole(req, res);
+  }
+
+  async UpdateRole(req: Request, res: Response): Promise<void> {
+    await RoleService.UpdateRole(req, res);
+  }
+
+  async DeleteRole(req: Request, res: Response): Promise<void> {
+    await RoleService.DeleteRole(req, res);
+  }
+}
+export default new RoleController();
---
+++ restaurant-backend/src/models/RoleModel.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import Permissions from "./PermissionModel";
+interface IRoles extends Document{
+    name: string;
+    decription: string | null;
+    permissions: string[];
+
+}
+
+const RoleSchema = new mongoose.Schema({
+  name: { type: String, require: true },
+  decription: { type: String},
+  permission: [{ type: String, ref: Permissions }],
+});
+
+const Roles = mongoose.model<IRoles>("roles", RoleSchema);
+export default Roles;  
