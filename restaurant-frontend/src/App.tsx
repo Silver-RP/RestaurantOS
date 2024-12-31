353c1e622644a7543a447cb13e926eb8ed75d460
Lam Gia Bao <bao9025@gmail.com>
2024-12-31 17:24:59 +0700

    update code sigg up

--- restaurant-frontend/src/App.tsx
+++ restaurant-frontend/src/App.tsx
@@ -1,14 +1,17 @@
-import './App.css'
-import React from 'react';
-import Header from './components/header/Header'
+import './App.css';
+// import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Register from './pages/Register';
 
 function App() {
 
   return (
-    <>
-      <Header></Header>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
