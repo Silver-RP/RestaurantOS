201e41dcf53dc1052a07368829feb334c6ae1648
Finnng1104 <tiennt1104@gmail.com>
2024-12-31 22:26:25 +0700

    header vs banner

--- restaurant-frontend/src/App.tsx
+++ restaurant-frontend/src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css'
 import React from 'react';
-import Header from './components/header/Header';
-import Footer from './components/footer/footer'
+import Header from './components/header/Header'
 
 function App() {
 
   return (
     <>
       <Header></Header>
-      <Footer></Footer>
     </>
   )
 }
