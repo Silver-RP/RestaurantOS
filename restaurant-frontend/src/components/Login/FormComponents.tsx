624266957afa7a78ea67131029f32da07b8c721b
Nguyen Ngoc My <nguyenngocmy1311@gmail.com>
2025-01-01 15:09:42 +0700

    update code signup

--- restaurant-frontend/src/components/Login/FormComponents.tsx
+++ restaurant-frontend/src/components/Login/FormComponents.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import type { FormProps } from 'antd';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Form } from 'antd';
 import InputComponent from './InputComponents';
 import ButtonComponent from './ButtonComponents';
-type Props = {};
 
-const FormComponent = (props: Props) => {
+
+const FormComponent = () => {
   type FieldType = {
     username?: string;
     password?: string;
@@ -24,41 +24,19 @@ const FormComponent = (props: Props) => {
   return (
     <div>
       <Form
-        name="basic"
         labelCol={{ span: 8 }}
         wrapperCol={{ span: 16 }}
         style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
         onFinish={onFinish}
         onFinishFailed={onFinishFailed}
-        autoComplete="off"
-        className='--ant-display:none'
+        
       >
-        <Form.Item<FieldType>
-          name="username"
-          rules={[{ required: true, message: 'Please input your username!' }]}
-        >   
-          <InputComponent />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <InputComponent />
-        </Form.Item>
-
-        {/* <Form.Item<FieldType>
-          name="remember"
-          valuePropName="checked"
-          label={null}
-        >
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item> */}
-
-        <Form.Item label={null}>
-            <ButtonComponent />
-        </Form.Item>
+          <InputComponent placeholder="Họ và tên" type="text" value="name" />
+          <InputComponent placeholder="Email / Số điện thoại" type="number" value="phone"/>
+          <InputComponent placeholder="Mật khẩu" type="password" value="password"/>
+          <InputComponent placeholder="Nhập lại mật khẩu" type="password" value="repassword"/>
+          <ButtonComponent />
+   
       </Form>
     </div>
   );
