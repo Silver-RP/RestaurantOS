2ee0282144073e7f69dae575c0837f3dc3c43d7b
Lam Gia Bao <bao9025@gmail.com>
2025-04-21 01:30:56 +0700

    add product-detail-page

--- restaurant-frontend/src/routers/index.tsx
+++ restaurant-frontend/src/routers/index.tsx
@@ -8,6 +8,7 @@ import ResetPassword from "../pages/ResetPassword";
 import ForgotPassword from "../pages/ForgotPassword";
 import EnterOTP from "../pages/EnterOTP";
 import ProfilePage from "../pages/Profile";
+import ProductDetail from "../pages/ProductDetail";
 // import AboutUs from "../pages/AboutUs";
 
 const AppRoutes = () => {
@@ -21,6 +22,7 @@ const AppRoutes = () => {
     { path: "/forgot-password", element: <ForgotPassword/> },
     { path: "/verify-otp", element: <EnterOTP/> },
     { path: "/profile", element: <ProfilePage/> },
+    { path: "/productdetail", element: <ProductDetail/> },
   ]);
   return routes;
 };
