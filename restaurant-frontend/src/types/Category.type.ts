6d298f7720cec307cca5bc49a4e732a9dcfc3e9a
Finnng1104 <tiennt1104@gmail.com>
2025-04-27 23:24:13 +0700

    add api cate

--- restaurant-frontend/src/types/Category.type.ts
+++ restaurant-frontend/src/types/Category.type.ts
@@ -6,3 +6,12 @@ export interface Category {
     Cate_img: string | null;
     parentCate: string | null;
   }
+
+// Đại diện cho response khi fetch all categories
+export interface CategoryResponse {
+  total: number;
+  page: number;
+  limit: number;
+  totalPages: number;
+  data: Category[];
+}
