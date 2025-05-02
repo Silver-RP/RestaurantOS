0239b2a47e5a014f66b5659e6c998f448b293775
silver-oss <thachlhps37716@fpt.edu.vn>
2025-05-02 11:25:45 +0700

    test husky 5

--- restaurant-backend/src/services/ReservationDetailContactService.ts
+++ restaurant-backend/src/services/ReservationDetailContactService.ts
@@ -22,10 +22,10 @@ class ReservationDetailContactService {
         .populate("foods", "name price countInStock");
         return reservationDetailContact;
     }
-
+    
     async updateReservationDetailContact (id: string, input: IReservationDetailContact): Promise<any> {
         const reservationDetailContact = await ReservationDetailContact.findByIdAndUpdate(id, input, {new: true});
-        return "abc";
+        return reservationDetailContact_2;
     }
     async deleteReservationDetailContact(id: string): Promise<any> {
         const reservationDetailContact = await ReservationDetailContact.findById(id);
