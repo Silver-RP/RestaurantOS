d3646f17a5ae1977e94fe7b2d54f754fe31f2109
silver-oss <thachlhps37716@fpt.edu.vn>
2025-05-02 12:27:39 +0700

    Refactor SampleModel and remove eslint-disable comments

--- restaurant-frontend/src/pages/EnterOTP.tsx
+++ restaurant-frontend/src/pages/EnterOTP.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
