8ee182373208f1f7c9618e0c00b8048bd90dbce2
Nguyen Ngoc My <nguyenngocmy1311@gmail.com>
2025-05-03 22:55:48 +0700

    update code

--- restaurant-backend/src/routes/CartRoutes.ts
+++ restaurant-backend/src/routes/CartRoutes.ts
@@ -4,6 +4,6 @@ import CartController from '../controller/CartController';
 const router = Router();
 
 router.put('/update/:id', CartController.UpdateCart); 
-router.delete('/:cartId/item/:dishId', CartController.DeleteCartItem);
-router.delete('/delete-all/:cartId', CartController.DeleteAllCart);
+router.delete('/:cartId/item/:dishId',CartController.DeleteCartItem);
+router.delete('/delete-all/:cartId',CartController.DeleteAllCart);
 export default router;
