10522e9c9fcd645e7b19e2c9fa2804e6ff698f3a
Finnng1104 <tiennt1104@gmail.com>
2025-06-09 22:40:23 +0700

    add error

--- restaurant-backend/src/config/mailer.ts
+++ restaurant-backend/src/config/mailer.ts
@@ -1,6 +1,5 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import nodemailer from 'nodemailer';
-import hbs from 'nodemailer-express-handlebars';
-// const hbs = await import('nodemailer-express-handlebars');
 import path from 'path';
 
 const transporter = nodemailer.createTransport({
@@ -11,47 +10,24 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-transporter.use(
-  'compile',
-  hbs({
-    viewEngine: {
-      extname: '.hbs',
-      partialsDir: path.resolve(__dirname, '../views'),
-      defaultLayout: false,
-    },
-    viewPath: path.resolve(__dirname, '../views'),
-    extName: '.hbs',
-  }),
-);
+// Initialize handlebars with dynamic import
+const initializeHandlebars = async () => {
+  const hbs = (await import('nodemailer-express-handlebars')).default;
+  transporter.use(
+    'compile',
+    hbs({
+      viewEngine: {
+        extname: '.hbs',
+        partialsDir: path.resolve(__dirname, '../views'),
+        defaultLayout: false,
+      },
+      viewPath: path.resolve(__dirname, '../views'),
+      extName: '.hbs',
+    }),
+  );
+};
+
+// Call the initialization
+initializeHandlebars().catch(console.error);
 
 export default transporter;
-
-// import nodemailer from 'nodemailer';
-// import path from 'path';
-
-// export const createTransporter = async () => {
-//   const { default: hbs } = await import('nodemailer-express-handlebars');
-
-//   const transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//       user: process.env.MAIL_USERNAME,
-//       pass: process.env.MAIL_PASSWORD,
-//     },
-//   });
-
-//   transporter.use(
-//     'compile',
-//     hbs({
-//       viewEngine: {
-//         extname: '.hbs',
-//         partialsDir: path.resolve(__dirname, '../views'),
-//         defaultLayout: false,
-//       },
-//       viewPath: path.resolve(__dirname, '../views'),
-//       extName: '.hbs',
-//     }),
-//   );
-
-//   return transporter;
-// };
