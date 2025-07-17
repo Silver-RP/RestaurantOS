5d76a30427dd9c1a268e60ca9b75d943e9a6d6d8
Lam Gia Bao <bao9025@gmail.com>
2025-07-17 11:28:59 +0700

    update loyalty

--- restaurant-backend/src/app.ts
+++ restaurant-backend/src/app.ts
@@ -39,7 +39,7 @@ import passport from 'passport';
 import cors from 'cors';
 import path from 'path';
 
-// Thêm dòng này để import và khởi động cron-job loyalty
+
 import { scheduleLoyaltyYearlyJob } from './cron/loyaltyYearlyJob';
 
 const app = express();
