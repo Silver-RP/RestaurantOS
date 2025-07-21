4c7602a321ed1fac08d0ca4bc8082279127c7819
daivonpham <phamquangdai0805@gmail.com>
2025-07-21 16:42:54 +0700

    udpate cors beefbeefrestaurant.io.vn

--- restaurant-backend/src/app.ts
+++ restaurant-backend/src/app.ts
@@ -63,7 +63,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'beefbeefrestaurant.io.vn',
     credentials: true,
   }),
 );
