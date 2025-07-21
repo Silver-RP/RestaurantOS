896459ad243c63bc549378edf7836205c4fdcbd6
daivonpham <phamquangdai0805@gmail.com>
2025-07-21 23:16:33 +0700

    update animation birthday

--- restaurant-frontend/src/components/common/BirthdayAnimation.tsx
+++ restaurant-frontend/src/components/common/BirthdayAnimation.tsx
@@ -2,6 +2,7 @@ import { useEffect, useCallback, useRef, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import Realistic from 'react-canvas-confetti/dist/presets/realistic';
 import Fireworks from 'react-canvas-confetti/dist/presets/fireworks';
+import GlobalModal from "@/components/common/GlobalModal";
 
 interface BirthdayAnimationProps {
   username: string;
@@ -48,77 +49,87 @@ const BirthdayAnimation = ({ username, onComplete, isVisible }: BirthdayAnimatio
   return (
     <AnimatePresence>
       {isVisible && visible && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-85">
-          <div className="absolute inset-0">
-            <Realistic
-              autorun={{ speed: 0.3 }}
-              onInit={({ conductor }) => (realisticConductorRef.current = conductor)}
-              style={{
-                position: 'absolute',
-                pointerEvents: 'none',
-                width: '100%',
-                height: '100%',
-                top: 0,
-                left: 0,
-              }}
-            />
-            <Fireworks
-              autorun={{ speed: 3 }}
-              onInit={({ conductor }) => (fireworksConductorRef.current = conductor)}
-              style={{
-                position: 'absolute',
-                pointerEvents: 'none',
-                width: '100%',
-                height: '100%',
-                top: 0,
-                left: 0,
-              }}
-            />
-          </div>
-          <div className="relative z-10">
-            <motion.div
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              exit={{ opacity: 0, y: -20 }}
-              transition={{ duration: 1 }}
-              className="text-center"
-            >
-              <motion.h1
-                initial={{ scale: 0.5 }}
-                animate={{ scale: 1 }}
-                transition={{
-                  type: 'spring',
-                  stiffness: 260,
-                  damping: 20,
-                  duration: 1.5,
+        <GlobalModal>
+          <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70">
+            <div className="absolute inset-0">
+              <Realistic
+                autorun={{ speed: 0.3 }}
+                onInit={({ conductor }) =>
+                  (realisticConductorRef.current = conductor)
+                }
+                style={{
+                  position: 'absolute',
+                  pointerEvents: 'none',
+                  width: '100%',
+                  height: '100%',
+                  top: 0,
+                  left: 0,
                 }}
-                className="text-5xl font-bold text-secondaryColor  mb-4"
-              >
-                🎉 Happy Birthday! 🎉
-              </motion.h1>
+              />
+              <Fireworks
+                autorun={{ speed: 3 }}
+                onInit={({ conductor }) =>
+                  (fireworksConductorRef.current = conductor)
+                }
+                style={{
+                  position: 'absolute',
+                  pointerEvents: 'none',
+                  width: '100%',
+                  height: '100%',
+                  top: 0,
+                  left: 0,
+                }}
+              />
+            </div>
+
+            <div className="relative z-10">
               <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.5, duration: 1 }}
-                className="text-3xl text-white font-semibold"
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -20 }}
+                transition={{ duration: 1 }}
+                className="text-center"
               >
-                {username}
-                <p className="mt-2 text-lg text-secondaryColor ">
-                  Chúc {username} luôn ngon miệng, vui vẻ và tràn đầy hương vị hạnh phúc!
-                </p>
+                <motion.img
+                  src="/assets/birthday/birthday.png"
+                  alt="Happy Birthday"
+                  initial={{ scale: 0.5 }}
+                  animate={{ scale: 1 }}
+                  transition={{
+                    type: 'spring',
+                    stiffness: 260,
+                    damping: 20,
+                    duration: 1.5,
+                  }}
+                  className="mb-4 w-[350px] h-[210px] object-cover mx-auto"
+                />
+
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.5, duration: 1 }}
+                  className="text-3xl text-white font-semibold"
+                >
+                  {username}
+                  <p className="mt-2 text-lg text-secondaryColor">
+                   Chúc {username} một ngày sinh nhật thật ý nghĩa,
+                    <br />
+                    tràn đầy hạnh phúc và thành công trong cuộc sống!
+                  </p>
+                </motion.div>
+                <motion.button
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 3 }}
+                  onClick={handleThankYouClick}
+                  className="mt-8 px-6 py-2 bg-secondaryColor  text-black rounded-full hover:bg-yellow-500 transition-colors"
+                >
+                  Thank you!
+                </motion.button>
               </motion.div>
-              <motion.button
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 3 }}
-                onClick={handleThankYouClick}
-                className="mt-8 px-6 py-2 bg-secondaryColor  text-black rounded-full hover:bg-yellow-500 transition-colors"
-              >
-                Thank you!
-              </motion.button>
-            </motion.div>
+            </div>
           </div>
-        </div>
+        </GlobalModal>
       )}
     </AnimatePresence>
   );
