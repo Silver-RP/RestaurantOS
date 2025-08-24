53a281bfc44b66f7d181d3efb29b9c813a280a4c
silver-oss <thachlhps37716@fpt.edu.vn>
2025-08-24 14:05:16 +0700

    Fix reservation user ID type, update payment method filtering, and correct import paths in AboutUs page.

--- restaurant-frontend/src/pages/AboutUs.tsx
+++ restaurant-frontend/src/pages/AboutUs.tsx
@@ -1,6 +1,6 @@
-import Experience from '../components/pages/aboutUs/experience/Experience';
-import Menter from '../components/pages/aboutUs/menter/Menter';
-import Menu from '../components/pages/aboutUs/menu/Menu';
+import Experience from '../components/pages/AboutUs/experience/Experience';
+import Menter from '../components/pages/AboutUs/menter/Menter';
+import Menu from '../components/pages/AboutUs/menu/Menu';
 
 import React from 'react';
 
